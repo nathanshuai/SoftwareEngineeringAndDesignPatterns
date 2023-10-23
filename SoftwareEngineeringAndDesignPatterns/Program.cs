33c700e303d3aa16050654a724bda25a971416f3
Nathan Shuai <nathan.shuai@gmail.com>
2023-10-22 20:37:43 -0500

    update

--- SoftwareEngineeringAndDesignPatterns/Program.cs
+++ SoftwareEngineeringAndDesignPatterns/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// initialize a base-level user
+User user = new User
+{
+    Username = "john_wiliam",
+    Email = "john@example.com"
+};
+
+// get base values
+Console.WriteLine(user.GetDescription());
+
+// then add decorators
+// Creating a badge decorator for reputation
+Client User = new ReputationBadgeDecorator(user);
+Console.WriteLine("After the user get Reputation Badge decorating");
+
+Console.WriteLine();
+// Displaying user information with badges
+Console.WriteLine(User.GetDescription());
+
+
+// ABSTRACT COMPONENT
+public abstract class Client
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+
+    protected string Description = "No Description";
+
+    public virtual string GetBadges()
+    {
+        return Description;
+    }
+
+    public virtual string GetDescription()
+    {
+        return Description;
+    }
+}
+
+// === CONCRETE COMPONENTS === 
+public class User : Client
+{
+    public User()
+    {
+        Description = "Base-level User";
+    }
+}
+
+// === ABSTRACT DECORATOR ===
+public abstract class BadgeDecorator : Client
+{
+    public Client Client { get; set; }
+
+    public abstract override string GetBadges();
+
+    public override string GetDescription()
+    {
+        // Append badges to the description
+        return $"{Client.GetDescription()} {GetBadges()}";
+    }
+
+    public BadgeDecorator(Client client)
+    {
+        Client = client;
+    }
+}
+
+// Concrete Decorator
+
+public class ReputationBadgeDecorator : BadgeDecorator
+{
+    public ReputationBadgeDecorator(Client client) : base(client)
+    {
+    }
+
+    public override string GetBadges()
+    {
+        // Simulating a reputation badge
+        return "Golden Badge";
+    }
+}
